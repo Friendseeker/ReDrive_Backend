f0a7143c9dbe154b10432b96f7ccfe8d6291515c
Jonathan Cheng <jcmohr9@gmail.com>
2021-07-17 18:42:57 -0700

    add city descriptions and url

--- backend.py
+++ backend.py
@@ -110,47 +110,76 @@ class HardCodedData:
     tempCitiesCanada = [sampleCity1, sampleCity2, sampleCity3]
     canadaCities = Cities(tempCitiesCanada, Country.CA)
 
-    no = City("New Orleans, Louisiana", apartmentList, "description", trumpImage)
-    sf = City("San Francisco, California", apartmentList, "description", trumpImage)
-    nyc = City("New York City, New York", apartmentList, "description", trumpImage)
-    chi = City("Chicago, Illinois", apartmentList, "description", trumpImage)
-    lv = City("Las Vegas, Nevada", apartmentList, "description", trumpImage)
-    la = City("Los Angeles, California", apartmentList, "description", trumpImage)
+    noDescription = "Anytime of year find live music, amazing Creole and Cajun cuisine, fresh seafood, farmers," \
+                    + "markets shopping, nightlife and more.  During Mardi Gras season, the city becomes the " \
+                      + "world’s center. No matter the time of year, New Orleans' calendar overflows in celebration."
+    sfDescription = "San Francisco is home to revelatory architecture, the first established LGBTQ+ neighborhood " \
+            + "in the country, and Michelin-starred dining. When in San Francisco dress in layers because the weather" \
+                    + " is constantly changing, and the fog often rolls in with little warning. Spend an afternoon" \
+                    + " lounging in a public park, taking advantage of San Francisco's seemingly endless green space." \
+                    + " Take the BART—i.e., the Metro—to dinner in the Mission. Hike along the Pacific Ocean, or at" \
+                    + " least stroll on Ocean Beach at sunset."
+    nycDescription = "One of the most popular tourist destinations in the world, NYC is the mecca of business in the " \
+                     + "United States, and a melting pot of American culture. NYC has something for every style," \
+                     + "taste and budget, and with so many hidden gems around every corner."
+    chiDescription = "Chicago has all the offerings you'd expect from a major city: world-class museums, vibrant " \
+                    + "shopping districts and ample nightlife venues, just to name a few. If you're here to learn," \
+                    +  " plan to spend a fair amount of time in Grant Park. This area is home to such notable " \
+                    + "institutions as the Art Institute of Chicago and The Field Museum. For a more Windy " \
+                    + "City-centric education, start your vacation with an architecture river cruise – which can" \
+                    + " provide background on Chicago's famous skyscrapers like the Willis Tower and Tribune Tower."
+    lvDescription = "Las Vegas is a city that was made for entertainment, carved out of the Mojave Desert with escape" \
+                    + "in mind. Millions of people visit Las Vegas annually to relax, dine, shop, " \
+                    + "see performers, and experience the nightlife."
+    laDescription = "L.A. is the entertainment capital of the world, a cultural mecca boasting more than 100 museums," \
+                    + " many of them world-class, and a paradise of idyllic weather. It is the only city in North " \
+                    + "America to have hosted the Summer Olympics twice - and by 2028 the third time. " \
+                    + "Downtown L.A. is the largest government center outside of Washington, D.C. Additionally, " \
+                    + "Los Angeles has the only remaining wooden lighthouse in the state (located in San " \
+                    + "Pedro’s Fermin Park) and the largest historical theater district on the National" \
+                    " Register of Historic Places (located Downtown on Broadway)."
+
+    no = City("New Orleans, Louisiana", apartmentList, noDescription, parse_url("https://tinyurl.com/9hz9neyn").url)
+    sf = City("San Francisco, California", apartmentList, sfDescription, parse_url("https://tinyurl.com/4uh8b8z4").url)
+    nyc = City("New York City, New York", apartmentList, nycDescription, parse_url("https://tinyurl.com/2ekk6ud3").url)
+    chi = City("Chicago, Illinois", apartmentList, chiDescription, parse_url("https://tinyurl.com/54fchxsc").url)
+    lv = City("Las Vegas, Nevada", apartmentList, lvDescription, parse_url("https://tinyurl.com/brusdza7").url)
+    la = City("Los Angeles, California", apartmentList, laDescription, parse_url("https://tinyurl.com/az45dsah").url)
     tempCitiesUS = [no, sf, nyc, chi, lv, la]
     usCities = Cities(tempCitiesUS, Country.US)
 
-    ro = City("Romes", apartmentList, "description", trumpImage)
-    ve = City("Venice", apartmentList, "description", trumpImage)
-    flo = City("Florence", apartmentList, "description", trumpImage)
-    mi = City("Milan", apartmentList, "description", trumpImage)
-    na = City("Naples", apartmentList, "description", trumpImage)
-    am = City("Amalfi", apartmentList, "description", trumpImage)
+    ro = City("Romes", apartmentList, "description", parse_url("").url)
+    ve = City("Venice", apartmentList, "description", parse_url("").url)
+    flo = City("Florence", apartmentList, "description", parse_url("").url)
+    mi = City("Milan", apartmentList, "description", parse_url("").url)
+    na = City("Naples", apartmentList, "description", parse_url("").url)
+    am = City("Amalfi", apartmentList, "description", parse_url("").url)
     tempCitiesItaly = [ro, ve, flo, mi, na, am]
     italyCities = Cities(tempCitiesItaly, Country.IT)
 
-    paris = City("Paris", [], "description", trumpImage)
-    nice = City("Nice", [], "description", trumpImage)
-    bordeaux = City("Bordeaux", [], "description", trumpImage)
-    lyon = City("Lyon", [], "description", trumpImage)
-    cannes = City("Cannes", [], "description", trumpImage)
-    corsica = City("Corsica", [], "description", trumpImage)
+    paris = City("Paris", [], "description", parse_url("").url)
+    nice = City("Nice", [], "description", parse_url("").url)
+    bordeaux = City("Bordeaux", [], "description", parse_url("").url)
+    lyon = City("Lyon", [], "description", parse_url("").url)
+    cannes = City("Cannes", [], "description", parse_url("").url)
+    corsica = City("Corsica", [], "description", parse_url("").url)
     tempCitiesFrance = [paris, nice, bordeaux, lyon, cannes, corsica]
     franceCities = Cities(tempCitiesFrance, Country.FR)
 
-    tokyo = City("Tokyo", [], "description", trumpImage)
-    kyoto = City("Kyoto", [], "description", trumpImage)
-    osaka = City("Osaka", [], "description", trumpImage)
-    hiroshima = City("Hiroshima", [], "description", trumpImage)
-    sapporo = City("Sapporo", [], "description", trumpImage)
-    fukuoka = City("Fukuoka", [], "description", trumpImage)
+    tokyo = City("Tokyo", [], "description", parse_url("").url)
+    kyoto = City("Kyoto", [], "description", parse_url("").url)
+    osaka = City("Osaka", [], "description", parse_url("").url)
+    hiroshima = City("Hiroshima", [], "description", parse_url("").url)
+    sapporo = City("Sapporo", [], "description", parse_url("").url)
+    fukuoka = City("Fukuoka", [], "description", parse_url("").url)
     tempCitiesJapan = [tokyo, kyoto, osaka, hiroshima, sapporo, fukuoka]
     japanCities = Cities(tempCitiesJapan, Country.JP)
 
-    beijing = City("Beijing", [], "description", trumpImage)
-    shanghai = City("Shanghai", [], "description", trumpImage)
-    chengdu = City("Chengdu", [], "description", trumpImage)
-    xian = City("Xi'an", [], "description", trumpImage)
-    hangzhou = City("Hanzhou", [], "description", trumpImage)
+    beijing = City("Beijing", [], "description", parse_url("").url)
+    shanghai = City("Shanghai", [], "description", parse_url("").url)
+    chengdu = City("Chengdu", [], "description", parse_url("").url)
+    xian = City("Xi'an", [], "description", parse_url("").url)
+    hangzhou = City("Hanzhou", [], "description", parse_url("").url)
     tempCitiesChina = [beijing, shanghai, chengdu, xian, hangzhou]
     chinaCities = Cities(tempCitiesChina, Country.CN)
 
